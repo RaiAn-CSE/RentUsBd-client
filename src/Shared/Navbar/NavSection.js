a0555a65da86cd190e38901fd5724c35e156017a
Md.Robiul Hasan <68548481+robiulxasan@users.noreply.github.com>
2023-01-06 23:31:25 +0600

    route change for add property

--- src/Shared/Navbar/NavSection.js
+++ src/Shared/Navbar/NavSection.js
@@ -57,7 +57,7 @@ const NavSection = () => {
                 </Link>
               </li>
 
-              {isBuyer === true ? (
+              {isBuyer || isAdmin === true ? (
                 <></>
               ) : (
                 <>
